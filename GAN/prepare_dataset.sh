51d5e75139234628f60171ecb2d9c105ebfea342
Christopher Aust <christopher.aust@student.hpi.uni-potsdam.de>
2021-09-02 12:31:34 +0200

    remove unused files in GAN

--- GAN/prepare_dataset.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-find $1 -mindepth 2 -type f -exec mv -t $1 -i '{}' +
-find $1 -type d -empty -delete
--- GAN/run_train_scratch.sh
+++
@@ -1,3 +0,0 @@
-#python3 clean.py
-export CUDA_VISIBLE_DEVICES=1
-python3 main_run.py 0
